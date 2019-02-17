6d78374dbd4bc6f990bb23d39758cffae0b56b97
mpaczocha <m_paczocha@o2.pl>
2019-02-17 18:25:40 +0100

    Commit1

--- components/AdsAppController.js
+++ components/AdsAppController.js
@@ -3,9 +3,10 @@
    angular
       .module("ngAdsApp")
       .controller("AdsAppController", function ($scope) {
-         $scope.name = {
-            first: "Ryan",
-            last: "Chunky"
-         };
+         $scope.ads = {
+            title: "First item",
+            price: "$1,000,000",
+            description: "Sint dolore consectetur aliqua pariatur eiusmod dolore labore tempor duis deserunt cillum dolor. Irure exercitation esse excepteur dolor fugiat nostrud sint magna incididunt in est. Est aliquip ullamco anim excepteur.Cillum aute irure ut qui id deserunt dolor adipisicing."
+         }
       });
 })();
--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,12 @@
 angular
-.module('ngAdsApp', ["ngMaterial"])
-.config(function($mdThemingProvider){
-   $mdThemingProvider.theme('default')
-   .primaryPalette('teal')
-   .accentPalette('orange');
-})
-.directive("helloWorld")
+   .module('ngAdsApp', ["ngMaterial"])
+   .config(function ($mdThemingProvider) {
+      $mdThemingProvider.theme('default')
+         .primaryPalette('teal')
+         .accentPalette('orange');
+   })
+   .directive("helloWorld", function () {
+      return {
+         template: "<h1>{{message}}</h1>"
+      }
+   });
